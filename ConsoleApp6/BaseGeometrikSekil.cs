c66758434ceb664f6eb25c8617d96eef98741fea
ENGİN <ENGN@ENGIN>
2024-08-13 16:29:22 +0300

    Add project files.

---
+++ ConsoleApp6/BaseGeometrikSekil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class BaseGeometrikSekil
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public virtual int AreaCalculate()
+        {
+            int sonuc = width * height;
+            return sonuc;
+            Console.WriteLine("Area is : "+sonuc);
+        }
+       
+    }
+    public class Square : BaseGeometrikSekil
+    {
+        public override int AreaCalculate()
+        {
+            return width * height;
+        }
+    }
+    public class Rectangle : BaseGeometrikSekil
+    {
+        public override int AreaCalculate()
+        {
+            return width * height;
+        }
+
+    }
+    public class RightTriangle : BaseGeometrikSekil
+    {
+        public override int AreaCalculate()
+        {
+            return (width * height) / 2;
+        }
+    }
+}
+
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using ConsoleApp6;
+
+Square square = new Square();
+
+square.width = 10;
+square.height = 10;
+Console.WriteLine("Area of Square : " + square.AreaCalculate());
+
+
+
+
+Rectangle rectangle = new Rectangle();
+
+
+RightTriangle triangle = new RightTriangle();
+
+Console.WriteLine("Area of rectangle: " + rectangle.AreaCalculate());
+Console.WriteLine("Area of Right-triangle: " + triangle.AreaCalculate());
